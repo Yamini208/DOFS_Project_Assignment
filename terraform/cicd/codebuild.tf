3a80fa089abaa3dccb8eab599d9446e52fd55e9a
Yamini208 <sriyamini208@gmail.com>
2025-07-16 15:54:23 +0530

    Update codebuild.tf

---
+++ terraform/cicd/codebuild.tf
@@ -0,0 +1,26 @@
+resource "aws_codebuild_project" "terraform_build" {
+  name          = "terraform-build"
+  service_role  = aws_iam_role.codebuild_role.arn
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:5.0"
+    type                        = "LINUX_CONTAINER"
+    environment_variables = [
+      {
+        name  = "AWS_REGION"
+        value = var.aws_region
+      }
+    ]
+    privileged_mode = true
+  }
+
+  source {
+    type            = "GITHUB"
+    location        = "https://github.com/${var.github_repo}.git"
+    buildspec       = "buildspec.yml"
+  }
+}
