924561976070179b458303b51943a0a639950e0c
Yamini208 <sriyamini208@gmail.com>
2025-07-16 15:55:02 +0530

    Update codepipeline.tf

---
+++ terraform/cicd/codepipeline.tf
@@ -0,0 +1,45 @@
+resource "aws_codepipeline" "terraform_pipeline" {
+  name     = "terraform-pipeline"
+  role_arn = aws_iam_role.codepipeline_role.arn
+
+  artifact_store {
+    location = aws_s3_bucket.artifact_store.bucket
+    type     = "S3"
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        Owner      = split("/", var.github_repo)[0]
+        Repo       = split("/", var.github_repo)[1]
+        Branch     = var.github_branch
+        OAuthToken = var.github_oauth_token
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name             = "TerraformBuild"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      input_artifacts  = ["source_output"]
+      version          = "1"
+      configuration = {
+        ProjectName = aws_codebuild_project.terraform_build.name
+      }
+    }
+  }
+}
