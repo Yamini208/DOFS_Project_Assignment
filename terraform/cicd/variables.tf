c1a2aaefbfb83604928d260999263d87e1eb0489
Yamini208 <sriyamini208@gmail.com>
2025-07-16 17:02:17 +0530

    Update variables.tf

--- terraform/cicd/variables.tf
+++ terraform/cicd/variables.tf
@@ -9,3 +9,9 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "project_name" {
+  description = "Name of the project (used for bucket naming)"
+  type        = string
+  default     = "order-fulfillment"
+}
