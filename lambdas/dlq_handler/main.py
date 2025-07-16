8b94d1f7c0a494b006b7ec038d5bc3f58a90d0d0
Yamini208 <sriyamini208@gmail.com>
2025-07-16 15:45:07 +0530

    Update main.py

--- lambdas/dlq_handler/main.py
+++ lambdas/dlq_handler/main.py
@@ -7,17 +7,17 @@ table_name = os.environ['FAILED_TABLE']
 table = dynamodb.Table(table_name)
 
 def lambda_handler(event, context):
-    print("✅ DLQ Lambda triggered")
-    print(f"📦 Raw event: {json.dumps(event)}")
+    print("DLQ Lambda triggered")
+    print(f" Raw event: {json.dumps(event)}")
 
     for record in event.get('Records', []):
         try:
             body = json.loads(record['body'])
             print(f"📄 Record body: {body}")
             response = table.put_item(Item=body)
-            print(f"📝 Writing to DynamoDB: {response}")
+            print(f" Writing to DynamoDB: {response}")
         except Exception as e:
-            print(f"❌ Error inserting to DynamoDB: {e}")
+            print(f" Error inserting to DynamoDB: {e}")
 
     return {
         'statusCode': 200,
